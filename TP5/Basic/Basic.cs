19ed1388180233be468912c7294d4ac08ac945ac
Benjamin Tora <benjamin.tora@epita.fr>
2021-10-27 17:54:02 +0200

    1

---
+++ TP5/Basic/Basic.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Basic
+{
+   
+    class Basic
+    {
+        static void Main(string[] args)
+        {
+
+            //PrintMe("testFile");
+            //MyLs("./");
+            //FillMe("testFile","test")
+            //CopyMe("tesstFile","testFmmile");
+            //enCryptoVernam("decrypted","AZDGKHHLMBHFPOTHATQYHCFHBOKBAAD","encrypted");
+            //deCryptoVernam("encrypted","AZDGKHHLMBHFPOTHATQYHCFHBOKBAAD","decrypted");
+        }
+
+        public static void PrintMe(string path)
+        {
+            // TODO
+
+            path = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Error : \"{path}\" does not exist");
+                return;
+            }
+
+            StreamReader sr = File.OpenText(path);
+            string s;
+            int line = 1;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine($"Line {line++}: {s}");
+            }
+
+            sr.Close();
+        }
+
+        public static void MyLs(string path)
+        {
+            string npath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            if (!Directory.Exists(npath))
+            {
+                Console.WriteLine($"Error: \"{path}\" is not a directory");
+                return;
+            }
+            List<string> dirs = new List<string>(Directory.GetFileSystemEntries(npath));
+
+            foreach (var dir in dirs)
+            {
+                Console.Write($"{dir.Substring(dir.LastIndexOf(Path.DirectorySeparatorChar) + 3)} ");
+            }
+        }
+
+        public static void FillMe(string path, string content)
+        {
+            string Npath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            if (Directory.Exists(Npath))
+            {
+                Console.WriteLine("Directory with the same name already exists");
+                return;
+
+            }
+            File.AppendAllText(Npath, string.Format("\n{0}", content));
+            Console.WriteLine($"Printed \"{content}\" successfully in \"{path}\"");
+
+        }
+
+        public static void CopyMe(string path, string dest)
+        {
+            string npath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            string ndest = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), dest);
+
+            if (!File.Exists(npath))
+            {
+                Console.WriteLine($"Error: \"{path}\" does not exist, abort mission");
+                return;
+            }
+            if (File.Exists(ndest))
+            {
+                Console.WriteLine($"Warning: The file \"{dest}\" already exists");
+                Console.WriteLine(" Do you want to overwrite it ?");
+                string res = Console.ReadLine();
+                switch (res.ToLower())
+                {
+                    case "yes":
+                    {
+                        File.Copy(npath,ndest,true);
+                        Console.WriteLine($"Copy from \"{path}\" to \"{dest}\" was successful");
+
+                        break;
+                    }
+                    case "no":
+                    {
+                        Console.WriteLine("Mission aborted successfully");
+                        return;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Error: bad input, you had one job...");
+                        return;
+                        
+                    }
+                    
+                }
+                return;
+            }
+            File.Copy(npath,ndest,true);
+            Console.WriteLine($"Copy from \"{path}\" to \"{dest}\" was successful");
+            
+        }
+
+        public static void enCryptoVernam(string decrypted, string key, string encrypted)
+        {
+            string path = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), decrypted);
+            string encryptedpath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), encrypted);
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Error : \"{decrypted}\" does not exist");
+                return;
+            }
+            StreamReader sr = File.OpenText(path);
+            string sourceText=sr.ReadLine();
+            sr.Close();
+            string encrypt = Encrypt_and_Decrypt(sourceText, key,true);
+            StreamWriter sw = new StreamWriter(File.Open(encryptedpath, FileMode.Create));
+            sw.WriteLine(encrypt);
+            sw.Close();
+
+
+        }
+
+        public static void deCryptoVernam(string encrypted, string key, string decrypted)
+        {
+            string path = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), encrypted);
+            string decryptedpath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), decrypted);
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Error : \"{decrypted}\" does not exist");
+                return;
+            }
+            StreamReader sr = File.OpenText(path);
+            string sourceText=sr.ReadLine();
+            sr.Close();
+            string encrypt = Encrypt_and_Decrypt(sourceText, key,false);
+            StreamWriter sw = new StreamWriter(File.Open(decryptedpath, FileMode.Create));
+            sw.WriteLine(encrypt);
+            sw.Close();
+            
+        }
+
+        private static int Modulo(int k, int n)
+        {
+            return ((k %= n) < 0) ? k + n : k;
+        }
+
+        private static string Encrypt_and_Decrypt(string sourcetext, string key,bool mode)
+        {
+            string[] sourceWord = sourcetext.ToUpper().Split();
+
+            char[] language = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            StringBuilder code = new StringBuilder();
+            int[] key_id = new int[key.Length];
+            
+            for (int i = 0; i < key.Length; i++)
+            {
+                for (int j = 0; j < language.Length; j++)
+                {
+                    if (key[i] == language[j])
+                    {
+                        key_id[i] = j;
+                        break;
+                    }
+                }
+            }
+
+            int indexInSourceText = 0;
+            for (int id = 0; id < sourceWord.Length; id++)
+            {
+                string word = sourceWord[id];
+            
+                if (id != 0)
+                {
+                    code.Append(" ");
+                }
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                
+                    for (int j = 0; j < language.Length; j++)
+                    {
+               
+                        if (word[i] == language[j])
+                        {
+                            if (mode)
+                            {
+                                code.Append(language[(j+key_id[indexInSourceText])%26]);
+                     
+                            }
+                            else
+                            {
+                                
+                              
+                                code.Append(language[Modulo(j-key_id[indexInSourceText],26)]);
+   
+                            }
+                            break;
+                        }
+                
+                        else if (j == language.Length - 1)
+                        {
+                            code.Append(word[i]);
+                        }
+                    }
+
+                    indexInSourceText++;
+                }
+
+            }
+            
+           
+
+            return code.ToString();
+        }
+    }
+}
---
+++ TP5/ParseMyCSV/ParseMyCSV.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ParseMyCSV
+{
+    public enum Houses
+    {
+        None,
+        Gryffindor,
+        Hufflepuff,
+        Ravenclaw,
+        Slytherin,
+    }
+
+    public class Student
+    {
+        public string Name;
+        public Houses House;
+        public int Points;
+
+        public Student(string name, Houses house, int points)
+        {
+            Name = name;
+            House = house;
+            Points = points;
+        }
+
+        public static void AssignHouse(Student student)
+        {
+            List<string> Gryffindor = new List<string>(){"Harry Potter", "Hermione Granger", "Ron Weasley"};            
+            List<string> Slytherin = new List<string>(){"Draco Malfoy"};
+            List<string> Hufflepuff = new List<string>(){"Ernie Macmillan"};
+            List<string> Ravenclaw = new List<string>(){"Terry Boot"};
+
+            if (Gryffindor.Contains(student.Name))
+            {
+                student.House = Houses.Gryffindor;
+                return;
+            }
+            if (Slytherin.Contains(student.Name))
+            {
+                student.House = Houses.Slytherin;
+                return;
+            }
+            if (Hufflepuff.Contains(student.Name))
+            {
+                student.House = Houses.Hufflepuff;
+                return;
+            }
+            if (Ravenclaw.Contains(student.Name))
+            {
+                student.House = Houses.Ravenclaw;
+                return;
+            }
+
+            student.House = Houses.None;
+
+        }
+    }
+
+    class ParseMyCSV
+    {
+        public static Dictionary<string, Student> CreateStudentsInfoFromFormat(string path)
+        {
+            path = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Error : \"{path}\" does not exist");
+                return null;
+            }
+
+            StreamReader sr = File.OpenText(path);
+            List<string> filesLine=new List<string>();
+            string contents;
+            while ((contents = sr.ReadLine()) != null)
+            {
+               filesLine.Add(contents);
+            }
+            sr.Close();
+            if (filesLine.Count == 0)
+            {
+                return null;
+            }
+            Dictionary<string, Student> dict = new Dictionary<string, Student>();
+            foreach (string line in filesLine)
+            {
+                
+                string[] data = line.Split(',');
+                if(data.Length!=3)
+                    continue;
+                Student student = new Student(data[0],(Houses)int.Parse(data[1]),int.Parse(data[2]));
+                if (student.House == Houses.None) Student.AssignHouse(student);
+                dict.Add(student.Name,student);
+            }
+
+            return dict;
+        }
+
+        public static void SaveStudents(Dictionary<string, Student> students, string dest)
+        {
+            if (students == null) return;
+            string destpath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), dest);
+
+            StreamWriter sw = new StreamWriter(File.Open(destpath, FileMode.Create));
+            foreach (KeyValuePair<string, Student> kvp in students)
+            {
+                sw.WriteLine($"{kvp.Value.Name}, { (int)kvp.Value.House}, {kvp.Value.Points}");
+            }
+            sw.Close();
+
+        }
+
+        public static void AddPoints(Dictionary<string, Student> students, string student, int point)
+        {
+            if (!students.ContainsKey(student))
+            {
+                Console.WriteLine($"Error: \"{student}\" does not exist");
+                return;
+            }
+
+            students[student].Points += point;
+        }
+
+        public static void GivePoints(Dictionary<string, Student> students)
+        {
+            List<string> HarryTeam = new List<string>(){"Harry Potter", "Hermione Granger", "Ron Weasley"};  
+            Random r = new Random();
+            foreach (var student in students)
+            {
+                student.Value.Points=r.Next(0, 99);
+                if (HarryTeam.Contains(student.Key))
+                {
+                    AddPoints(students,student.Key,99999);
+                }
+            }
+            
+        }
+
+        public static void WinnerOfTheHouseCup(Dictionary<string, Student> students)
+        {
+            int[] list = new int[5];
+            foreach (var student in students)
+            {
+                if (student.Value.House != Houses.None)
+                {
+                    list[(int) student.Value.House] += student.Value.Points;
+                }   
+            }
+
+            int maxValue = list.Max();
+            int maxIndex = list.ToList().IndexOf(maxValue);
+
+            Console.WriteLine($"Winner of the House Cup is : {((Houses)maxIndex).ToString()}");
+
+        }
+
+        public static void ListofHouses(Dictionary<string, Student> students, string dest)
+        {
+            string destpath = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), dest);
+            StreamWriter sw = new StreamWriter(File.Open(destpath, FileMode.Create));
+
+            sw.WriteLine("House Gryffindor : ");
+            foreach (KeyValuePair<string, Student> kvp in students.Where(x => x.Value.House == Houses.Gryffindor))
+            {
+                sw.WriteLine(kvp.Key);
+            }
+            sw.WriteLine("\n\nHouse Hufflepuff :");
+            foreach (KeyValuePair<string, Student> kvp in students.Where(x => x.Value.House == Houses.Hufflepuff))
+            {
+                sw.WriteLine(kvp.Key);
+            }
+            sw.WriteLine("\n\nHouse Ravenclaw :");
+            foreach (KeyValuePair<string, Student> kvp in students.Where(x => x.Value.House == Houses.Ravenclaw))
+            {
+                sw.WriteLine(kvp.Key);
+            }
+            sw.WriteLine("\n\nHouse Slytherin :");
+            foreach (KeyValuePair<string, Student> kvp in students.Where(x => x.Value.House == Houses.Slytherin))
+            {
+                sw.WriteLine(kvp.Key);
+            }
+            sw.Close();
+            
+        }
+
+        public static void Update(Dictionary<string, Student> students, string path)
+        {
+            path = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\")), path);
+            StreamReader sr = File.OpenText(path);
+            string contents;
+            while ((contents = sr.ReadLine()) != null)
+            {
+                string[] data = contents.Split('/');
+                if(data.Length<2) 
+                    continue;
+                switch (data[0])
+                {
+                    case "RenameStudent":
+                    {
+                        foreach (var student in students)
+                        {
+                            if (student.Key == data[1])
+                            {
+                                student.Value.Name = data[2];
+                                Student newStudent = new Student(data[2], student.Value.House, student.Value.Points);
+                                students.Remove(student.Key);
+                                students.Add(data[2],newStudent);
+                                break;
+                            }
+                        }
+
+                        break;
+                    }
+                    case "ChangeHouse":
+                    {
+                        foreach (var student in students)
+                        {
+                            if (student.Key == data[1])
+                            {
+                                student.Value.House = (Houses) int.Parse(data[2]);
+                            }
+                        }
+                        break;
+                    }
+                    case "AddStudent":
+                    {
+                        Student newStudent = new Student(data[1],(Houses) int.Parse(data[2]), int.Parse(data[2]));
+                        students.Add(data[1],newStudent);
+                        break;
+                    }
+                    case "RemoveStudent":
+                    {
+                        students.Remove(data[1]);
+                        break;
+                    }
+                }
+            }
+
+            sr.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Dictionary<string,Student>students=CreateStudentsInfoFromFormat("student.csv");
+            GivePoints(students);
+            WinnerOfTheHouseCup(students);
+            ListofHouses(students,"HousesList");
+            Update(students,"change");
+            SaveStudents(students,"student.csv");
+
+        }
+    }
+}
